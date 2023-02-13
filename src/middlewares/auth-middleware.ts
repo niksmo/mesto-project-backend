6809e81142ef25f509bdd54b9c5c71e5829daebf
Nikolay Smolov <n.smolov@yandex.ru>
2023-02-13 16:46:29 +0300

    refactor auth middleware type approach

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -1,20 +1,16 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { RequestWithUser } from '../controllers/controllers-types';
 import ApiError from '../exceptions/api-error';
 import tokenService from '../services/token-service';
 
-function authMiddleware(
-  req: RequestWithUser,
-  Res: Response,
-  next: NextFunction
-) {
+function authMiddleware(req: Request, Res: Response, next: NextFunction) {
   const { accessToken } = req.cookies;
 
   if (accessToken) {
     try {
       const userId = tokenService.validateToken(accessToken);
 
-      req.user = { _id: userId };
+      (req as RequestWithUser).user = { _id: userId };
       next();
     } catch (error) {
       next(error);
